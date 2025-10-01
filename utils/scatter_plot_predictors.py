916417a6bf31ee553855690f5d1c1cb7730cab93
Gabriel <gabriel.amorim.melo@gmail.com>
2025-10-01 12:15:03 -0300

    Atualizando os comentários da função

--- utils/scatter_plot_predictors.py
+++ utils/scatter_plot_predictors.py
@@ -5,19 +5,19 @@ from pandas.plotting import scatter_matrix
 
 def scatter_plot_predictors(df : pd.core.frame.DataFrame, target: str, number_of_plots: int) -> None:
     """
-    Shows a matrix of scatter plots between the most related variables of the DataFrame
+    Shows a matrix of scatter plots between the most related variables to a target column of the DataFrame
 
     Args:
         df (pd.core.frame.DataFrame): The DataFrame on which we will operate
         target (String): The column of the DataFrame that is the target variable
-        number_of_plots (int): How many variables are going to be selected to plot 
+        number_of_plots (int): How many correlated variables are going to be selected to plot 
 
     Returns:
         None
     """
     
     # Given that we do not know how many attributes the DataFrame has it is better to only display the plots of a given set of variables. 
-    # We decide which of them are going to be shown using correlation coefficient and then select the given number of attributes
+    # We decide which of them are going to be shown using the correlation coefficient and then select the given number of attributes
     corr_matrix = df.corr()
     corr_list = corr_matrix[target].sort_values(ascending=False)
     attributes = corr_list.index[:number_of_plots].tolist()
