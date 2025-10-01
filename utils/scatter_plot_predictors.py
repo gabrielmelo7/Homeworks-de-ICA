dc5e026ce534de45f71a1a5ba75de3937bf6321a
Gabriel <gabriel.amorim.melo@gmail.com>
2025-10-01 10:48:00 -0300

    Adicionando alguns comentários melhores

--- utils/scatter_plot_predictors.py
+++ utils/scatter_plot_predictors.py
@@ -10,14 +10,14 @@ def scatter_plot_predictors(df : pd.core.frame.DataFrame, target: str, number_of
     Args:
         df (pd.core.frame.DataFrame): The DataFrame on which we will operate
         target (String): The column of the DataFrame that is the target variable
-        number_of_plots (int): How many scatter plots are going to be shown
+        number_of_plots (int): How many variables are going to be selected to plot 
 
     Returns:
         None
     """
     
     # Given that we do not know how many attributes the DataFrame has it is better to only display the plots of a given set of variables. 
-    # We decide which of them are going to be shown using correlation coefficient and then select the givn number of attributes
+    # We decide which of them are going to be shown using correlation coefficient and then select the given number of attributes
     corr_matrix = df.corr()
     corr_list = corr_matrix[target].sort_values(ascending=False)
     attributes = corr_list.index[:number_of_plots].tolist()
