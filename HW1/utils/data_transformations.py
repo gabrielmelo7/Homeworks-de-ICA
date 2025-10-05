b8f56c9799b3e9d27471accd97480ca7f2d97628
Filipe Caetano <filipekaetano04@gmail.com>
2025-10-05 16:49:00 -0300

    polishing the implementation of the transformations

--- HW1/utils/data_transformations.py
+++ HW1/utils/data_transformations.py
@@ -15,22 +15,37 @@ def standard_zscore(data : pd.DataFrame) -> pd.DataFrame:
 
     return (data - data.mean()) / data.std()
 
-def boxcox_transform(data : pd.DataFrame) -> pd.DataFrame:
+def boxcox_transform(data : pd.DataFrame) -> tuple[pd.DataFrame, np.ndarray, np.ndarray]:
     """
-    Applies the Box-Cox transformation to each column of the DataFrame.
+    Applies the Box-Cox transformation to each column of a DataFrame, handling non-positive values through a shift.
 
     Args:
         data (pd.DataFrame): The input DataFrame.
 
     Returns:
-        pd.DataFrame: A new DataFrame with the transformed data.
+        Tuple(pd.DataFrame, np.ndarray, np.ndarray):
+            - The new DataFrame with the transformed data.
+            - An array with the lambda values found for each column.
+            - An array with the offset (shift) values applied to each column.
     """
 
+    number_columns = len(data.columns)
+    shift = np.zeros(number_columns)
+    lambda_values = np.zeros(number_columns)
+
     transformed_data = data.copy()
-    for col in transformed_data.columns:
+    for i, col in enumerate(transformed_data.columns):
+        
+        min_value = transformed_data[col].min()
+        if min_value <= 0:
+            shift[i] = abs(min_value) + 1 
+        
+        shifted_data = transformed_data[col] + shift[i]
+
         # Returns the column after the transformation and the lambda that best fits
-        transformed_data[col], _ = stats.boxcox(transformed_data[col])
-    return transformed_data
+        transformed_data[col], lambda_values[i]  = stats.boxcox(shifted_data)
+
+    return transformed_data, lambda_values, shift
 
 def yeojohnson_transform(data : pd.DataFrame) -> pd.DataFrame:
     """
