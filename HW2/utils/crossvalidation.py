497fd8ba8f10a97bd9756ec007808fe94b5f13e3
Vinícius Lôbo <140026897+Viniciusalp35@users.noreply.github.com>
2025-12-06 17:28:03 -0300

    FIX: fixed crossvalidation based on new module Ridge

--- HW2/utils/crossvalidation.py
+++ HW2/utils/crossvalidation.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
-from rmse import root_mean_square_error
-from Ridge_regression import Ridge_regression_train, predicted_ridge
-from r_squared import r2
+from utils.rmse import root_mean_square_error
+from utils.ridge_regression_module import Ridge_regression
+from utils.r_squared import r2
 
 def cross_validation_ridge(x_train:pd.DataFrame,y_train:pd.DataFrame,lamb,folds=10):
+    np.random.seed(42)
     seq = np.arange(x_train.shape[0])
     np.random.shuffle(seq)
     x_train_random = x_train.copy().iloc[seq]
@@ -18,13 +19,16 @@ def cross_validation_ridge(x_train:pd.DataFrame,y_train:pd.DataFrame,lamb,folds=
         for i in range(folds):
             temp_x = np.concatenate(x_train_subsets[:i] + x_train_subsets[i+1:])
             temp_y = np.concatenate(y_train_subsets[:i] + y_train_subsets[i+1:])
-            b,_= Ridge_regression_train(temp_x, temp_y,k)
-            y_predicted = predicted_ridge(x_train_subsets[i],b)
+            b= Ridge_regression.Ridge_regression_train(temp_x, temp_y,k)
+            y_predicted = Ridge_regression.predicted_ridge_static(x_train_subsets[i],b)
             RMSE = root_mean_square_error(y_predicted, y_train_subsets[i]); RMSE_results.append(RMSE)
             R2 = r2(y_predicted, y_train_subsets[i]); r2_results.append(R2)
         mean_rmse = np.mean(RMSE_results); mean_r2 = np.mean(r2_results)
         results.append((mean_rmse,mean_r2,k,b))
     final = sorted(results, key=lambda x: x[1],reverse=True)
-    final_df = pd.DataFrame(final,columns=["Mean_RMSE","Mean_r2","lambda","betha"])
+    final_df = pd.DataFrame(final,columns=["Mean_RMSE","Mean_r2","lamb","betha"])
 
     return final_df
+
+if __name__ == "__main__":
+    print("hello")
