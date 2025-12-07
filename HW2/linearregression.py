7a557a1f6344cf723c697fb047617788426c1edd
Gabriel <gabriel.amorim.melo@gmail.com>
2025-12-07 16:59:48 -0300

    Changes to linear regression file and cross val

--- HW2/linearregression.py
+++ HW2/linearregression.py
@@ -3,6 +3,7 @@ from utils.olsregression import ols_regression
 from utils.train_test_split import train_test_split 
 from utils.rmse import root_mean_square_error
 from utils.r_squared import r2 
+from utils.crossvalidation import cross_validation
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 
@@ -17,16 +18,18 @@ map_values = {
 
 data_df['Air Quality'].replace(map_values, inplace=True)
 
+data_df.head()
+
+data_df.info()
+
 x_train_set, x_test_set, y_train_set, y_test_set = train_test_split(data_df, "SO2")
 
-ols_model = ols_regression(x_train_set, y_train_set)
+ols_model = ols_regression()
+ols_model.fit(x_train_set, y_train_set)
 model = LinearRegression()
 model.fit(x_train_set, y_train_set)
 
-ones = np.ones((x_test_set.shape[0], 1))
-x_test_aug = np.hstack([ones, x_test_set])
-
-ols_predictions = x_test_aug @ ols_model 
+ols_predictions = ols_model.predict(x_test_set) 
 model_predictions = model.predict(x_test_set)
 
 ols_rmse = root_mean_square_error(ols_predictions, y_test_set)
@@ -37,3 +40,19 @@ model_r2 = r2(model_predictions, y_test_set)
 print(f"Ols rmse= {ols_rmse}, Scikit-learn rmse= {model_rmse}\n")
 print(f"Ols r squared= {ols_r2}, Scikit-learn r squared= {model_r2}\n")
 print(f"Ols weights = {ols_model}\n Scikit-learn weights= {np.hstack([model.intercept_, model.coef_])}\n")
+
+# K-fold cross validation of the OLS model
+rmse, rmse_std, r2, r2_std = cross_validation(data_df, "SO2", ols_model, 5)
+
+print(f"Cross val rmse: {rmse}\n")
+print(f"Cross val rmse_std: {rmse_std}\n")
+print(f"Cross val r2: {r2}\n")
+print(f"Cross val r2_std: {r2_std}\n")
+
+# K-fold cross validation of the scikit model
+rmse, rmse_std, r2, r2_std = cross_validation(data_df, "SO2", model, 5)
+
+print(f"Scikit-learn rmse: {rmse}\n")
+print(f"Scikit-learn rmse_std: {rmse_std}\n")
+print(f"Scikit-learn r2: {r2}\n")
+print(f"Scikit-learn r2_std: {r2_std}\n")
